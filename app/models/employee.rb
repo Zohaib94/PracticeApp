40ea8def1da006d09df671dffbe858ce0edee9cb
Zohaib94 <zohaibahmedbutt@gmail.com>
2016-06-12 21:24:46 +0500

    Implemented Association Callbacks

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,25 @@
 class Employee < ActiveRecord::Base
+=begin
+
+	CALLBACKS:
+		before_add
+		after_add
+		before_remove
+		after_remove
+
+=end
+
   has_many :subordinates, class_name: "Employee",
-                          foreign_key: "manager_id"
+                          foreign_key: "manager_id" ,
+                          after_add: :check_subordinate
  
   belongs_to :manager, class_name: "Employee"
   attr_accessible :name
+
+  def check_subordinate(subordinate)
+  	unless subordinate.manager_id
+  		puts "Manager ID Not Found"
+  	end
+  end
+  
 end
